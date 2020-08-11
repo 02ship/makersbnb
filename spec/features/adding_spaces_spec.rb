5db9d48b326fe3034e1f9cf30068e44e269776ea
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-08-11 16:40:10 +0100

    Added new insertions description and price

--- spec/features/adding_spaces_spec.rb
+++ spec/features/adding_spaces_spec.rb
@@ -2,8 +2,11 @@ feature 'in add spaces page' do
   scenario 'add space' do
     visit('/spaces/add')
     fill_in :name, with: 'Room_1'
-    expect(page).to have_field('name')
+    fill_in :description, with: 'Description_1'
+    fill_in :price, with: '50'
     click_button 'Add'
     expect(page).to have_content('Room_1')
+    expect(page).to have_content('Description_1')
+    expect(page).to have_content('50')
   end
 end
