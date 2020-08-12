d37b5adc9b1ab3a224d86bb61d128907c0c733d9
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-08-12 11:34:47 +0100

    update spaces_page_spec

--- spec/features/spaces_page_spec.rb
+++ spec/features/spaces_page_spec.rb
@@ -1,6 +1,10 @@
 feature 'Spaces page' do
   scenario 'User goes to spaces page, and sees list of spaces' do
+    DatabaseConnection.query("INSERT INTO spaces(name, description, price) VALUES('Room_1', 'Description_1', '50');")
     visit('/spaces')
     expect(page).to have_content('Spaces:')
+    expect(page).to have_content('Room_1')
+    expect(page).to have_content('Description_1')
+    expect(page).to have_content('50')
   end
 end
