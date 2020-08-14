fe558f2bfa68a18cb56a15b98896d0e1e5fad6d8
Student <student@Makerss-Air.lan>
2020-08-14 11:21:26 +0100

    display start and end date, price step by 1, add button renamed to list a space

--- spec/features/adding_spaces_spec.rb
+++ spec/features/adding_spaces_spec.rb
@@ -4,7 +4,7 @@ feature 'in add spaces page' do
     fill_in :name, with: 'Room_1'
     fill_in :description, with: 'Description_1'
     fill_in :price, with: 50
-    click_button 'Add'
+    click_button 'List my space'
     expect(page).to have_content('Room_1')
     expect(page).to have_content('Description_1')
     expect(page).to have_content('50')
